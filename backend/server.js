9d7cab30f578da50bd2bf1f9c5aa69f05e6eec2f
Vijayakumar-Harish <vijayakumarharisha@gmail.com>
2026-01-04 23:40:35 +0530

    fix: update cors

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type"]
 }));
 
-app.options("*", cors()); // 🔥 REQUIRED for preflight
+// app.options("*", cors()); // 🔥 REQUIRED for preflight
 
 // -------------------- Routes --------------------
 
